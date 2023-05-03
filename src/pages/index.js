df226133a74ac2a7f403845cf89d2ed003e00d15
CheatSetha <cheatsetha2001@gmail.com>
2023-05-03 09:58:30 +0700

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,36 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import MovieCard from '@/components/MovieCard'
+import { API_KEY } from '@/lib'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ data }) {
+  const movies = data?.results || []
+	console.log(movies)
   return (
-    <main >
-      
-    </main>
-  )
+		<div className='w-10/12 mx-auto '>
+			<div className='flex flex-wrap gap-4 mb-5'>
+				{movies.map((movie) => (
+					<MovieCard
+						key={movie.id}
+						id={movie.id}
+						title={movie.title}
+						thumbnail={movie.backdrop_path}
+						popularity={movie.popularity}
+					/>
+				))}
+			</div>
+		</div>
+	)
+}
+// getserver side props
+export async function getServerSideProps(context) {
+	const res = await fetch(
+		`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=1%60`
+	)
+	const data = await res.json()
+	return {
+		props: { data }, // will be passed to the page component as props
+	}
 }
